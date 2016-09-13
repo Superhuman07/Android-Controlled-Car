3e74bd9801be71c9b991c8b7fe357d80bc863085
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-13 21:00:32 +0530

    Add Comments

--- Arduino Code/Robo.ino
+++ Arduino Code/Robo.ino
@@ -14,29 +14,36 @@ int right = 12;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LmotorUp,OUTPUT);//Setting ports as OUTPUTS as they 
-  pinMode(LmotorDn,OUTPUT);//supply signal values to motor driver
+  // Setting ports as OUTPUTS as they supply signal values to motor driver
+  pinMode(LmotorUp,OUTPUT); 
+  pinMode(LmotorDn,OUTPUT);
   pinMode(RmotorUp,OUTPUT);
   pinMode(RmotorDn,OUTPUT);
   pinMode(left,OUTPUT);
   pinMode(right,OUTPUT);
-  Serial.println("<Robot name> Welcomes you"); //begining statement because i can :P
+  // Begining statement because i can :P add your robot name in the print statement
+  Serial.println("<Robot name> Welcomes you!"); 
+  // A delay of 1 second for user 
   delay(1000);
 } 
   
 
 void loop()
-{
-  if(Serial.available()>0)//Checking is Serial data available
+{ 
+  // Checking is Serial data available
+  if(Serial.available()>0)
   {
-    int input = Serial.read(); //Storing value of read data into variable assigned
+    // Storing value of read data into variable assigned
+    int input = Serial.read(); 
     switch(input)
     {
-     case 'w' : MoveF(); //Calling respective functions if mathced with case label 
+      // Calling respective functions if mathced with case label 
+     case 'w' : MoveF(); 
      break;
-     case 's' : MoveB(); // here is it important to use '' as that stores the ASCII value is the letter in the variable
+     // Here is it important to use '' as that stores the ASCII value is the letter in the variable you can add CAPITAL letters 
+     case 's' : MoveB(); 
      break;
-     case 'a' : MoveL(); // and hence we can use the int keyword
+     case 'a' : MoveL(); 
      break;
      case 'd' : MoveR();
      break;
@@ -50,23 +57,31 @@ void loop()
 
 void MoveF()
 {
+  // Telling user that the robot will move forward
   Serial.println("<Robot Name> Forward");
   
-  
-  digitalWrite(LmotorUp,HIGH); //As said in video we are writing a Digital HIGH to the upper left pin of the motor driver
-  digitalWrite(LmotorDn,LOW);//As said in video we are writing a Digital LOW to the lower left pin of the motor driver
-  digitalWrite(RmotorUp,HIGH); //As said in video we are writing a Digital HIGH to the upper right pin of the motor driver
+  // We are writing a Digital HIGH to the upper left pin of the motor driver
+  digitalWrite(LmotorUp,HIGH); 
+  // We are writing a Digital LOW to the lower left pin of the motor driver
+  digitalWrite(LmotorDn,LOW);
+  // We are writing a Digital HIGH to the upper right pin of the motor driver
+  digitalWrite(RmotorUp,HIGH); 
+  // We are writing a Digital HIGH to the lower right pin of the motor driver
   digitalWrite(RmotorDn,LOW);
   digitalWrite(left, 100);
-  digitalWrite(right, 100);//As said in video we are writing a Digital LOW to the lower right pin of the motor driver
+  // We are writing a Digital LOW to the lower right pin of the motor driver
+  digitalWrite(right, 100);
   
   // As a result the robot will move forward
 }
 
 void MoveB()
 {
+  // Telling user that the robot will move backward
   Serial.println("<Robot Name> Backward");
-  digitalWrite(LmotorUp,LOW); //Reverse logic of above
+  
+  // Reversing the Digital HIGH/LOW for all the pins of the moto driver exact opposite of the forward function MoveF
+  digitalWrite(LmotorUp,LOW); 
   digitalWrite(LmotorDn,HIGH);
   digitalWrite(left, 100);
   digitalWrite(RmotorUp,LOW);
@@ -77,33 +92,50 @@ void MoveB()
 
 void MoveL()
 {
+  // Telling user that the robot will move left
   Serial.println("<Robot Name> Left");
-  digitalWrite(LmotorUp,LOW); //we are completely stopping the left motor and allowing the right motor to move forward
+  // Completely stopping the left motor and allowing the right motor to move forward
+  digitalWrite(LmotorUp,LOW); 
   digitalWrite(LmotorDn,LOW);
-  digitalWrite(right, 00);//this will cause the robot to move left
+  // This will cause the robot to move left as the left motor will not move
+  digitalWrite(right, 00);
+  
   digitalWrite(RmotorUp,HIGH);
   digitalWrite(RmotorDn,LOW);
   digitalWrite(left, 100);
+  
+  // Result robot will move left as left motor is not moving
 }
 
 void MoveR()
 {
+  // Telling user that the robot will move right
   Serial.println("<Robot Name> Right");
-  digitalWrite(LmotorUp,HIGH);//we are completely stopping the right motor and allowing the left motor to move forward
-  digitalWrite(LmotorDn,LOW);//this will cause the robot to move right
+  
+  // Completely stopping the right motor and allowing the left motor to move forward
+  digitalWrite(LmotorUp,HIGH);
+  digitalWrite(LmotorDn,LOW);
+  // This will cause the robot to move right
   digitalWrite(RmotorUp,LOW);
   digitalWrite(RmotorDn,LOW);
   digitalWrite(right, 100);
   digitalWrite(left, 00);
+  
+  // Result robot will move right as right motor is not moving
 }
 
 void Stop()
 {
+  // Telling user that the robot will move right
   Serial.println("<Robot Name> Stop");
-  digitalWrite(LmotorUp,LOW);//Writing a digital LOW to all pins associated with movement of motor driver causes all motors to stop
+  
+  //Writing a digital LOW to all pins associated with movement of motor driver causes all motors to stop
+  digitalWrite(LmotorUp,LOW);
   digitalWrite(LmotorDn,LOW);
   digitalWrite(RmotorUp,LOW);
   digitalWrite(RmotorDn,LOW);
   digitalWrite(right, 00);
   digitalWrite(left, 00);
+  
+  // Result robot will not move anywhere
 }
